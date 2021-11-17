c70a4bf16c082832ce8e218e102170e48212aefc
alexey <alexeyweb60@gmail.com>
2021-11-17 22:19:29 +0200

    Initial Commit

---
+++ diabbetanddieta5/diabbetanddieta5App.swift
@@ -0,0 +1,17 @@
+//
+//  diabbetanddieta5App.swift
+//  diabbetanddieta5
+//
+//  Created by alexey on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabbetanddieta5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
